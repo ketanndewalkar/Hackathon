be2ccd111ff2b7010576b9dc5a56f19123dcc951
Rushi23442 <waghrushi228@gmail.com>
2026-02-23 13:25:00 +0530

    added dashboard controller

--- Backend/src/routes/dashboard.routes.js
+++ Backend/src/routes/dashboard.routes.js
@@ -1,5 +1,7 @@
 import express from 'express';
+import { getDashboardData } from '../controllers/dashboard.controllers';
 
 const router = express.Router()
+router.get('/',getDashboardData)
 
 export default router
